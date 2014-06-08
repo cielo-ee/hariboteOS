b304be7d3b890d1b30b8722321be902dc9750573
cielo-ee <cielo.ee+github@gmail.com>
2014-06-08 11:12:30 +0900

    色情報を追加

--- bootpack.c
+++ bootpack.c
@@ -1,11 +1,24 @@
 void io_hlt(void);
+void io_cli(void);                /*clear interrupt flag */
 void write_mem8(int addr,int data);
+void io_out8(int port,int data); /*port番号で指定した装置にdataを送りつける*/
+int  io_load_eflags(void);
+void io_store_eflags(int eflags);
+
+void init_palette(void);
+void set_palette(int start,int end,unsigned char *rgp);
 
 void HariMain(void)
 {
 		int i;
-		for(i = 0xa000; i <= 0xafffff; i++){
-				write_mem8(i,15);
+		char *p;
+
+		init_palette();
+
+		p = (char *)0xa0000;
+		
+		for(i = 0; i <= 0xffff; i++){
+				p[i] = i & 0x0f;
 		}
 
 		for(;;){
@@ -13,3 +26,45 @@ void HariMain(void)
 		}
 }
 
+void init_palette(void){
+		static unsigned char table_rgb[16 * 3] = {
+				0x00,0x00,0x00, /* 0:黒 */
+				0xff,0x00,0x00, /* 1:明るい赤 */
+				0x00,0xff,0x00, /* 2:明るい緑*/
+				0xff,0xff,0x00, /* 3:明るい黄色*/
+				0xff,0x00,0xff, /* 5:明るい紫*/
+				0x00,0xff,0xff, /* 6:明るい水色*/
+				0x00,0xff,0xff, /* 7:白*/
+				0xc6,0xc6,0xc6, /* 8:明るい灰色*/
+				0x84,0x00,0x00, /* 9:暗い赤*/
+				0x00,0x84,0x00, /* 10:暗い緑 */
+				0x84,0x84,0x00, /* 11:暗い黄色*/
+				0x84,0x84,0x00, /* 12:暗い青*/
+				0x00,0x00,0x84, /* 13:暗い紫*/
+				0x00,0x84,0x84, /* 14:暗い水色*/
+				0x84,0x84,0x84, /* 15:暗い灰色*/
+					
+		};
+		set_palette(0,15,table_rgb);
+		return;
+
+		/* static charはDB命令のようなもの*/
+}
+
+void set_palette(int start, int end,unsigned char *rgb)
+{
+		int i,eflags;
+		eflags = io_load_eflags();
+
+		io_cli();
+		io_out8(0x03c8,start);
+
+		for(i = start;i <= end; i++){
+				io_out8(0x03c9,rgb[0] /4);
+				io_out8(0x03c9,rgb[1] /4);
+				io_out8(0x03c9,rgb[2] /4);
+				rgb += 3;
+		}
+		io_store_eflags(eflags); /* 割り込み許可フラグを元に戻す*/
+		return;
+}
