5f2b5ce02ecf7a482596119318330051e723c4ff
cielo-ee <cielo.ee+github@gmail.com>
2014-06-11 17:56:30 +0900

    割り込み処理を追加

--- bootpack.c
+++ bootpack.c
@@ -1,21 +1,29 @@
 /* bootpackのメイン */
 
 #include "bootpack.h"
+#include <stdio.h>
 
 void HariMain(void)
 {
-		struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
-		char s[40];
+		struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
+		char s[40],mcursor[256];
+		int mx, my;
+		
+		init_gdtidt();
+		init_pic();
+		io_sti; /* CPUの割り込み禁止を解除*/
 		
 		init_palette();
-		init_screen8(binfo->vram,binfo->scrnx,binfo->scrny);
+		init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
+		mx = (binfo->scrnx - 16) / 2; /* 画面中央になるように座標計算 */
+		my = (binfo->scrny - 28 - 16) / 2;
+		init_mouse_cursor8(mcursor, COL8_008484);
+		putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+		sprintf(s, "(%d, %d)", mx, my);
+		putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 		
-		putfonts8_asc(binfo->vram,binfo->scrnx,8,8,COL8_FFFFFF,"ABC 123");
-		putfonts8_asc(binfo->vram,binfo->scrnx,31,31,COL8_000000,"haribote OS");
-		putfonts8_asc(binfo->vram,binfo->scrnx,30,30,COL8_FFFFFF,"haribote OS");
-
-		sprintf(s, "*scrnx = %d", binfo->scrnx);
-		putfonts8_asc(binfo->vram,binfo->scrnx,16,64,COL8_FFFFFF,s);
+		io_out8(PIC0_IMR, 0xf9); /* PIC1とキーボードを許可(11111001) */
+		io_out8(PIC1_IMR, 0xef); /* マウスを許可(11101111) */
 		
 		for(;;){
 				io_hlt();
