43be4bb47ad6af08daea58f18f18ac1da4dd4fc9
cielo-ee <cielo.ee+github@gmail.com>
2014-07-30 11:53:21 +0900

    マウスからの割り込みを受け取り

--- int.c
+++ int.c
@@ -27,16 +27,15 @@ void init_pic(void)
 
 #define PORT_KEYDAT		0x0060
 
-/* struct KEYBUF keybuf; */
 struct FIFO8 keyfifo;
 
 void inthandler21(int *esp)
 {
-		unsigned char data;
-		io_out8(PIC0_OCW2,0x61);	/* IRQ-01受付完了をPICに通知 */
-		data = io_in8(PORT_KEYDAT);
-		fifo8_put(&keyfifo,data);
-		return;
+	unsigned char data;
+	io_out8(PIC0_OCW2, 0x61);	/* IRQ-01受付完了をPICに通知 */
+	data = io_in8(PORT_KEYDAT);
+	fifo8_put(&keyfifo, data);
+	return;
 }
 
 void inthandler2c(int *esp)
